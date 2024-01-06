5bd366da3865e59fe98a9c672bbf4655e47a7b35
Marcin Skic <marcinskicoficjal@gmail.com>
2024-01-06 15:52:43 +0100

    🖌️ Scale text slider with font size and texts length

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,10 @@
 <script>
-    import TextSlider from "./TextSlider.svelte";
+    import Header from "./Header.svelte";
     import "./styles.css";
-
-    const descriptions = [
-        "Web Developer",
-        "Game Developer",
-        "Technology Enthusiast",
-    ];
 </script>
 
 <div class="app">
-    <header>
-        <h1>
-            Hi, <br />
-            I am <span class="name">Marcin</span> <br />
-            <TextSlider texts={descriptions} duration={1000} />
-        </h1>
-    </header>
+    <Header --font-size="4rem" />
 
     <main>
         <slot />
@@ -38,27 +26,5 @@
         width: 100%;
         max-width: 64rem;
         margin: 0 auto;
-    }
-
-    header {
-        display: grid;
-        grid-template:
-            "title photo" auto
-            "nav nav" auto / auto auto;
-        place-content: center;
-        height: 100vh;
-        background-color: #0f172a;
-
-        > h1 {
-            grid-area: title;
-            min-width: 500px;
-            font-size: 4rem;
-            color: white;
-            text-align: start;
-
-            .name {
-                color: #eab308;
-            }
-        }
     }
 </style>
--- src/routes/TextSlider.svelte
+++ src/routes/TextSlider.svelte
@@ -3,6 +3,8 @@
     export let duration: number;
 
     let counter = 0;
+    const longestText =
+        (texts.toSorted((a, b) => a.length - b.length).pop()?.length ?? 0) / 2;
 
     const timeout = setInterval(() => {
         counter++;
@@ -12,12 +14,16 @@
     $: hiddenIndex = (counter + 1) % texts.length;
 </script>
 
-<span>
+<div class="text-slider" style:--longest-text={longestText}>
     <span aria-hidden="true" class="hidden">{texts[hiddenIndex]}</span>
     <span>{texts[currentIndex]}</span>
-</span>
+</div>
 
 <style lang="scss">
+    .text-slider {
+        min-width: calc(var(--font-size) * var(--longest-text));
+    }
+
     .hidden {
         display: none;
     }
