8f0cd87466ef754ebbfdcda640873d9a4352e248
Marcin Skic <marcinskicoficjal@gmail.com>
2024-01-09 11:45:42 +0100

    🖌️ Stop textSlider from working when not visible

--- src/routes/TextSlider.svelte
+++ src/routes/TextSlider.svelte
@@ -4,10 +4,11 @@
 
     export let texts: string[];
     export let duration: number;
+    let visibilityState: "visible" | "hidden";
 
     const displayedCounter = spring(0, {});
     const longestText =
-        (texts.toSorted((a, b) => a.length - b.length).pop()?.length ?? 0) / 2;
+        ([...texts].sort((a, b) => a.length - b.length).pop()?.length ?? 0) / 2;
 
     let counter = 0;
 
@@ -16,10 +17,12 @@
     $: hiddenIndex = Math.floor($displayedCounter + 1) % texts.length;
 
     setInterval(() => {
+        if (visibilityState === "hidden") return;
         counter++;
     }, duration);
 </script>
 
+<svelte:document bind:visibilityState />
 <div class="text-slider" style:--longest-text={longestText}>
     <div
         class="text-slider-content"
