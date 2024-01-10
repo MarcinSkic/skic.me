1e5ccdec2be0092d66c97b3ef1aa0ba9e1c86c49
Marcin Skic <marcinskicoficjal@gmail.com>
2024-01-10 01:51:56 +0100

    ✨ Add navigation bar to header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -6,10 +6,21 @@
         "Game Developer",
         "Technology Enthusiast",
     ];
+
+    const links = [
+        {
+            href: "#projects",
+            title: "Projects",
+        },
+        {
+            href: "#contact",
+            title: "Contact",
+        },
+    ];
 </script>
 
-<header>
-    <h1>
+<header class="header">
+    <h1 class="header__title">
         Hi, <br />
         I am <span class="name">Marcin</span> <br />
         <TextSlider
@@ -18,19 +29,32 @@
             animationDirection="right"
         />
     </h1>
+    <div class="header__photo"><img src="/" alt="person" /></div>
+    <nav>
+        <ul class="header__links">
+            {#each links as link}
+                <li>
+                    <a class="header__links__link" href={link.href}
+                        >{link.title}</a
+                    >
+                </li>
+            {/each}
+        </ul>
+    </nav>
 </header>
 
 <style lang="scss">
-    header {
+    .header {
         display: grid;
         grid-template:
             "title photo" auto
             "nav nav" auto / auto auto;
+        gap: 3rem 1rem;
         place-content: center;
         height: 100vh;
         background-color: #0f172a;
 
-        > h1 {
+        &__title {
             grid-area: title;
             font-size: var(--font-size);
             color: white;
@@ -38,6 +62,32 @@
 
             .name {
                 color: #eab308;
+            }
+        }
+
+        &__photo {
+            grid-area: photo;
+        }
+
+        &__links {
+            display: flex;
+            gap: var(--font-size);
+            align-items: center;
+            padding: 0;
+            margin: 0;
+            font-size: calc(var(--font-size) / 1.6);
+            list-style: none;
+
+            &__link {
+                background-image: linear-gradient(white 0 0);
+                background-repeat: no-repeat;
+                background-position: 200% 100%;
+                background-size: 200% 0.08em;
+                transition: background-position 0.3s;
+
+                &:hover {
+                    background-position: 100% 100%;
+                }
             }
         }
     }
