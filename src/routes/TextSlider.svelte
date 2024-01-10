d7beaf820158158fe2a3fb2b081d4e0a855d8535
Marcin Skic <marcinskicoficjal@gmail.com>
2024-01-10 01:24:08 +0100

    🐛 Stop slider from catching up after page inactivity

--- src/routes/TextSlider.svelte
+++ src/routes/TextSlider.svelte
@@ -3,7 +3,7 @@
     import { writable } from "svelte/store";
 
     export let texts: string[];
-    export let duration: number;
+    export let duration: number; //in milliseconds
     export let animationDirection: "left" | "right";
 
     const displayedCounter = spring(0, {});
@@ -12,33 +12,37 @@
 
     let counter = 0;
     let visibilityState: "visible" | "hidden";
+    let lastSlide = Date.now();
 
     $: displayedCounter.set(counter);
     $: currentIndex = Math.floor($displayedCounter) % texts.length;
     $: hiddenIndex = Math.floor($displayedCounter + 1) % texts.length;
+    $: offset = `${
+        ($displayedCounter % 1) *
+        100 *
+        (animationDirection === "right" ? 1 : -1)
+    }%`;
+    $: hiddenTextStyle =
+        animationDirection === "right" ? "left: -100%" : "right: -100%";
 
     setInterval(() => {
-        if (visibilityState === "hidden") return;
+        const skipSliding =
+            visibilityState === "hidden" ||
+            Date.now() - lastSlide < duration * 0.9;
 
+        if (skipSliding) return;
+
+        lastSlide = Date.now();
         counter += 1;
     }, duration);
 </script>
 
 <svelte:document bind:visibilityState />
 <div class="text-slider" style:--longest-text={longestText}>
-    <div
-        class="text-slider-content"
-        style:--offset="{($displayedCounter % 1) *
-            100 *
-            (animationDirection === "right" ? 1 : -1)}%"
-    >
-        <span
-            aria-hidden="true"
-            class="hidden"
-            style={animationDirection === "right"
-                ? "left: -100%"
-                : "right: -100%"}>{texts[hiddenIndex]}</span
-        >
+    <div class="text-slider-content" style:--offset={offset}>
+        <span aria-hidden="true" class="hidden" style={hiddenTextStyle}>
+            {texts[hiddenIndex]}
+        </span>
         <span>{texts[currentIndex]}</span>
     </div>
 </div>
