1ba1bd4272f96fe5d268d7da2b2f5fbe998bf7de
Marcin Skic <marcinskicoficjal@gmail.com>
2024-02-22 13:16:02 +0100

    ✨ Add experience metadata

---
+++ src/lib/experience.ts
@@ -0,0 +1,31 @@
+interface Experience {
+    began_at: Date;
+    finished_at?: Date;
+    description: string;
+    roles: string[];
+    category: string;
+}
+
+export const experience: Experience[] = [
+    {
+        began_at: new Date("2022-11-01"),
+        finished_at: new Date("2024-01-01"),
+        description: `I created mobile idle game <a href="#laser_inc_idle_miner">Laser Inc. Idle Miner</a> together with one person for a contract`,
+        roles: ["developer", "designer"],
+        category: "gamedev",
+    },
+    {
+        began_at: new Date("2020-12-01"),
+        finished_at: new Date("2021-10-01"),
+        description: `I took part in an "arena" course and developed mobile game prototypes based on existing designs`,
+        roles: ["developer"],
+        category: "gamedev",
+    },
+    {
+        began_at: new Date("2019-12-01"),
+        finished_at: new Date("2020-11-01"),
+        description: `During high school, I took part as a team in the T3G national competition where we created the video game <a href="#sara">Sara</a>`,
+        roles: ["developer", "designer"],
+        category: "gamedev",
+    },
+];
