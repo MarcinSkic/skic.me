afa97edaca0b90ead5e9bc21a7d794e8f63aa011
MarcinSkic <marcinskicoficjal@gmail.com>
2024-09-17 17:59:29 +0200

    feat: update projects metadata

--- src/lib/projects.ts
+++ src/lib/projects.ts
@@ -1,8 +1,9 @@
 import portfolio1 from "$lib/images/projects/portfolio/header.png";
 import portfolio2 from "$lib/images/projects/portfolio/projects.png";
-import name_that_tune1 from "$lib/images/projects/name_that_tune/selection.png";
-import name_that_tune2 from "$lib/images/projects/name_that_tune/game.png";
-import name_that_tune3 from "$lib/images/projects/name_that_tune/guessed.png";
+import harmonify_1 from "$lib/images/projects/harmonify/setup.png";
+import harmonify_2 from "$lib/images/projects/harmonify/round.png";
+import harmonify_3 from "$lib/images/projects/harmonify/roundResult.png";
+import harmonify_4 from "$lib/images/projects/harmonify/result.png";
 import laser_inc_idle_miner1 from "$lib/images/projects/laser_inc_idle_miner/game.webp";
 import laser_inc_idle_miner2 from "$lib/images/projects/laser_inc_idle_miner/lasers_shop.webp";
 import laser_inc_idle_miner3 from "$lib/images/projects/laser_inc_idle_miner/upgrades_shop.webp";
@@ -17,193 +18,218 @@ import sara3 from "$lib/images/projects/sara/fight.png";
 import sara4 from "$lib/images/projects/sara/crafting.png";
 import type { MatrixElement } from "../routes/flowingGrid/math";
 
+export const technologies = {
+  svelte:
+    "https://img.shields.io/badge/Svelte-4A4A55?style=for-the-badge&logo=svelte&logoColor=FF3E00",
+  typescript:
+    "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
+  next: "https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white",
+  scss: "https://img.shields.io/badge/Scss-CC6699?style=for-the-badge&logo=sass&logoColor=white",
+  unity:
+    "https://img.shields.io/badge/Unity-100000?style=for-the-badge&logo=unity&logoColor=white",
+  csharp:
+    "https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white",
+  mongodb:
+    "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white",
+  express: "https://img.shields.io/badge/Express.js-404D59?style=for-the-badge",
+  tailwind:
+    "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
+  vue: "https://img.shields.io/badge/Vue.js-35495E?style=for-the-badge&logo=vue.js&logoColor=4FC08D",
+  dotnet:
+    "https://img.shields.io/badge/.NET-5C2D91?style=for-the-badge&logo=.net&logoColor=white",
+} as const;
+
+type Techs = keyof typeof technologies;
+
 export interface Project {
-    id: string;
-    name: string;
-    description: string;
-    imagesAspectRatio: number;
-    images: {
-        alt: string;
-        src: string;
-    }[];
-    technologies: string[];
-    url?: string;
+  id: string;
+  name: string;
+  description: string;
+  imagesAspectRatio: number;
+  images: {
+    alt: string;
+    src: string;
+  }[];
+  technologies: Techs[];
+  urls: {
+    site?: string;
     github?: string;
-    rows?: number;
+    yt?: string;
+  };
+  rows?: number;
 }
 
 export const projects: MatrixElement<Project>[] = [
-    {
-        item: {
-            id: "laser_inc_idle_miner",
-            name: "Laser Inc. Idle Miner",
-            description:
-                "Idle game where user mines with lasers to earn as much money as he can, also physics based!",
-            imagesAspectRatio: 0.5625,
-            images: [
-                {
-                    alt: "Game view",
-                    src: laser_inc_idle_miner1,
-                },
-                {
-                    alt: "Lasers shop",
-                    src: laser_inc_idle_miner2,
-                },
-                {
-                    alt: "Upgrades shop",
-                    src: laser_inc_idle_miner3,
-                },
-            ],
-            technologies: ["unity", "csharp"],
-            url: "https://play.google.com/store/apps/details?id=laser.inc.idle.miner&hl=en_US",
+  {
+    item: {
+      id: "laser_inc_idle_miner",
+      name: "Laser Inc. Idle Miner",
+      description:
+        "Idle game where user mines with lasers to earn as much money as he can, also physics based!",
+      imagesAspectRatio: 0.5625,
+      images: [
+        {
+          alt: "Game view",
+          src: laser_inc_idle_miner1,
         },
-        rows: 2,
-    },
-    {
-        item: {
-            id: "digishelf",
-            name: "Digishelf",
-            description:
-                'My engineering project on subject: "Application for collecting information about popculture"',
-            imagesAspectRatio: 0.98,
-            images: [
-                {
-                    alt: "Library page",
-                    src: digishelf_1,
-                },
-                {
-                    alt: "Work creation form",
-                    src: digishelf_2,
-                },
-                {
-                    alt: "Reports page",
-                    src: digishelf_3,
-                },
-            ],
-            technologies: ["next", "express", "mongodb", "typescript", "scss"],
-            url: "https://digishelf-app.vercel.app",
-            github: "https://github.com/MarcinSkic/digishelf",
+        {
+          alt: "Lasers shop",
+          src: laser_inc_idle_miner2,
         },
-        rows: 1,
-    },
-    {
-        item: {
-            id: "name_that_tune",
-            name: "Name that tune",
-            description:
-                "App where user can guess tracks from his playlists and albums on Spotify",
-            imagesAspectRatio: 1.263157895,
-            images: [
-                {
-                    alt: "Tracks selection view",
-                    src: name_that_tune1,
-                },
-                {
-                    alt: "Round view",
-                    src: name_that_tune2,
-                },
-                {
-                    alt: "Track guessed modal",
-                    src: name_that_tune3,
-                },
-            ],
-            technologies: ["next", "typescript", "scss"],
-            url: "https://name-that-tune.vercel.app",
-            github: "https://github.com/MarcinSkic/name-that-tune",
+        {
+          alt: "Upgrades shop",
+          src: laser_inc_idle_miner3,
         },
-        rows: 1,
+      ],
+      technologies: ["unity", "csharp"],
+      urls: {
+        github: "https://github.com/MarcinSkic/laser-inc-idle-miner",
+      },
     },
-    {
-        item: {
-            id: "portfolio",
-            name: "Portfolio",
-            description: "Website you are currently looking at",
-            imagesAspectRatio: 1.03,
-            images: [
-                {
-                    alt: "Header",
-                    src: portfolio1,
-                },
-                {
-                    alt: "Projects view",
-                    src: portfolio2,
-                },
-            ],
-            technologies: ["svelte", "typescript", "scss"],
-            url: "htpps://skic.me",
-            github: "https://github.com/MarcinSkic/portfolio-website",
+    rows: 2,
+  },
+  {
+    item: {
+      id: "digishelf",
+      name: "Digishelf",
+      description:
+        'My engineering project on subject: "Application for collecting information about popculture"',
+      imagesAspectRatio: 0.98,
+      images: [
+        {
+          alt: "Library page",
+          src: digishelf_1,
         },
-        rows: 1,
-    },
-    {
-        item: {
-            id: "readme_tech_generator",
-            name: "Readme Tech Generator",
-            description:
-                "Website for generating summary of the technology used in repository, suitable for README footer",
-            imagesAspectRatio: 1.03,
-            images: [
-                {
-                    alt: "Tech list",
-                    src: readmetech1,
-                },
-                {
-                    alt: "Generated markdown showcase",
-                    src: readmetech2,
-                },
-            ],
-            technologies: ["svelte", "typescript", "tailwind"],
-            url: "https://marcinskic.github.io/readme-tech-generator/",
-            github: "https://github.com/MarcinSkic/readme-tech-generator",
+        {
+          alt: "Work creation form",
+          src: digishelf_2,
         },
-        rows: 1,
-    },
-    {
-        item: {
-            id: "sara",
-            name: "Sara",
-            description:
-                "Desktop game developed by a high school team for T3G: National Game Development Competition",
-            imagesAspectRatio: 1.777,
-            images: [
-                {
-                    alt: "Main menu",
-                    src: sara1,
-                },
-                {
-                    alt: "Landscape view",
-                    src: sara2,
-                },
-                {
-                    alt: "Fighting robot screenshot",
-                    src: sara3,
-                },
-                {
-                    alt: "Crafting view",
-                    src: sara4,
-                },
-            ],
-            technologies: ["unity", "csharp"],
-            url: "https://t3g.pl/bez-kategorii/znamy-zwyciezcow-turnieju-trojgamicznego/",
+        {
+          alt: "Reports page",
+          src: digishelf_3,
         },
-        rows: 1,
+      ],
+      technologies: ["next", "express", "mongodb", "typescript", "scss"],
+      urls: {
+        site: "https://digishelf-app.vercel.app",
+        github: "https://github.com/MarcinSkic/digishelf",
+      },
     },
+    rows: 1,
+  },
+  {
+    item: {
+      id: "harmonify",
+      name: "Harmonify",
+      description:
+        "Web game where users can guess tracks from their playlists and albums on Spotify",
+      imagesAspectRatio: 1.35,
+      images: [
+        {
+          alt: "Game setup view",
+          src: harmonify_1,
+        },
+        {
+          alt: "Round view",
+          src: harmonify_2,
+        },
+        {
+          alt: "Round result view",
+          src: harmonify_3,
+        },
+        {
+          alt: "Game result view",
+          src: harmonify_4,
+        },
+      ],
+      technologies: ["vue", "typescript", "tailwind", "dotnet", "csharp"],
+      urls: {
+        site: "https://harmonify-tune.vercel.app/",
+        github: "https://github.com/kaczkadevteam/harmonify",
+      },
+    },
+    rows: 1,
+  },
+  {
+    item: {
+      id: "portfolio",
+      name: "Portfolio",
+      description: "Website you are currently looking at",
+      imagesAspectRatio: 1.03,
+      images: [
+        {
+          alt: "Header",
+          src: portfolio1,
+        },
+        {
+          alt: "Projects view",
+          src: portfolio2,
+        },
+      ],
+      technologies: ["svelte", "typescript", "scss"],
+      urls: {
+        site: "https://skic.me",
+        github: "https://github.com/MarcinSkic/portfolio-website",
+      },
+    },
+    rows: 1,
+  },
+  {
+    item: {
+      id: "readme_tech_generator",
+      name: "Readme Tech Generator",
+      description:
+        "Website for generating summary of the technology used in repository, suitable for README footer",
+      imagesAspectRatio: 1.03,
+      images: [
+        {
+          alt: "Tech list",
+          src: readmetech1,
+        },
+        {
+          alt: "Generated markdown showcase",
+          src: readmetech2,
+        },
+      ],
+      technologies: ["svelte", "typescript", "tailwind"],
+      urls: {
+        site: "https://marcinskic.github.io/readme-tech-generator/",
+        github: "https://github.com/MarcinSkic/readme-tech-generator",
+      },
+    },
+    rows: 1,
+  },
+  {
+    item: {
+      id: "sara",
+      name: "Sara",
+      description:
+        "Desktop game developed by a high school team for T3G: National Game Development Competition",
+      imagesAspectRatio: 1.777,
+      images: [
+        {
+          alt: "Main menu",
+          src: sara1,
+        },
+        {
+          alt: "Landscape view",
+          src: sara2,
+        },
+        {
+          alt: "Fighting robot screenshot",
+          src: sara3,
+        },
+        {
+          alt: "Crafting view",
+          src: sara4,
+        },
+      ],
+      technologies: ["unity", "csharp"],
+      urls: {
+        yt: "https://www.youtube.com/live/P9iaFoaSOXI?t=4313s",
+        site: "https://t3g.pl/bez-kategorii/znamy-zwyciezcow-turnieju-trojgamicznego/",
+      },
+    },
+    rows: 1,
+  },
 ];
-
-export const technologies: {
-    [key: string]: string;
-} = {
-    svelte: "https://img.shields.io/badge/Svelte-4A4A55?style=for-the-badge&logo=svelte&logoColor=FF3E00",
-    typescript:
-        "https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white",
-    next: "https://img.shields.io/badge/Next-black?style=for-the-badge&logo=next.js&logoColor=white",
-    scss: "https://img.shields.io/badge/Scss-CC6699?style=for-the-badge&logo=sass&logoColor=white",
-    unity: "https://img.shields.io/badge/Unity-100000?style=for-the-badge&logo=unity&logoColor=white",
-    csharp: "https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white",
-    mongodb:
-        "https://img.shields.io/badge/MongoDB-4EA94B?style=for-the-badge&logo=mongodb&logoColor=white",
-    express:
-        "https://img.shields.io/badge/Express.js-404D59?style=for-the-badge",
-    tailwind:
-        "https://img.shields.io/badge/Tailwind_CSS-38B2AC?style=for-the-badge&logo=tailwind-css&logoColor=white",
-};
--- src/routes/project/Project.svelte
+++ src/routes/project/Project.svelte
@@ -1,200 +1,208 @@
 <script context="module" lang="ts">
-    let toShow = 0;
+  let toShow = 0;
 </script>
 
 <script lang="ts">
-    import { technologies, type Project } from "$lib/projects";
-    import ImageSlider from "../sliders/ImageSlider.svelte";
-    import github from "$lib/images/github.svg?raw";
-    import { afterUpdate, onMount } from "svelte";
-    import { fly } from "svelte/transition";
-    import { page } from "$app/stores";
+  import { technologies, type Project } from "$lib/projects";
+  import ImageSlider from "../sliders/ImageSlider.svelte";
+  import github from "$lib/images/github.svg?raw";
+  import { afterUpdate, onMount } from "svelte";
+  import { fly } from "svelte/transition";
+  import { page } from "$app/stores";
 
-    export let imageSlideDelay: number;
-    export let project: Project;
+  export let imageSlideDelay: number;
+  export let project: Project;
 
-    function showIfVisible() {
-        if (scrolledTo) return;
+  function showIfVisible() {
+    if (scrolledTo) return;
 
-        const rect = element.getBoundingClientRect();
+    const rect = element.getBoundingClientRect();
 
-        if (rect.top + visibleHeightToShow < windowInnerHeight) {
-            scrolledTo = true;
+    if (rect.top + visibleHeightToShow < windowInnerHeight) {
+      scrolledTo = true;
 
-            setTimeout(() => {
-                visible = true;
-                toShow -= 1;
-            }, toShow * 150);
+      setTimeout(() => {
+        visible = true;
+        toShow -= 1;
+      }, toShow * 150);
 
-            toShow += 1;
-        }
+      toShow += 1;
     }
+  }
 
-    const duration = Math.random() * 2000 + 4000;
-    /** Value in px */
-    const visibleHeightToShow = 100;
-    let scrolledTo = false;
-    let visible = false;
-    let imageSlider: any;
-    let imageSliderHeight = 300;
-    let element: HTMLDivElement;
-    let windowInnerHeight: number;
+  const duration = Math.random() * 2000 + 4000;
+  /** Value in px */
+  const visibleHeightToShow = 100;
+  let scrolledTo = false;
+  let visible = false;
+  let imageSlider: any;
+  let imageSliderHeight = 300;
+  let element: HTMLDivElement;
+  let windowInnerHeight: number;
 
-    $: selected = $page.url.hash === `#${project.id}`;
+  $: selected = $page.url.hash === `#${project.id}`;
 
-    onMount(() => {
-        imageSliderHeight = imageSlider.offsetWidth / project.imagesAspectRatio;
-        showIfVisible();
-    });
+  onMount(() => {
+    imageSliderHeight = imageSlider.offsetWidth / project.imagesAspectRatio;
+    showIfVisible();
+  });
 
-    afterUpdate(() => {
-        if (imageSlider) {
-            imageSliderHeight =
-                imageSlider.offsetWidth / project.imagesAspectRatio;
-        }
-    });
+  afterUpdate(() => {
+    if (imageSlider) {
+      imageSliderHeight = imageSlider.offsetWidth / project.imagesAspectRatio;
+    }
+  });
 </script>
 
 <svelte:window
-    on:scroll={() => {
-        showIfVisible();
-    }}
-    bind:innerHeight={windowInnerHeight}
+  on:scroll={() => {
+    showIfVisible();
+  }}
+  bind:innerHeight={windowInnerHeight}
 />
 
 {#key visible}
-    <div
-        id={project.id}
-        class="project"
-        class:project--active={selected}
-        style:--visible={visible ? "visible" : "hidden"}
-        in:fly={{ x: -200, duration: 800 }}
-        bind:this={element}
-    >
-        <h3 class="project__title">{project.name}</h3>
-        <div class="project__icons">
-            {#if project.github}
-                <a
-                    class="project__icons__icon project__icons__icon--github"
-                    href={project.github}
-                    target="_blank"
-                    rel="noopener"
-                >
-                    {@html github}
-                </a>
-            {/if}
-            {#if project.url}
-                <a href={project.url} target="_blank" rel="author noopener">
-                    <iconify-icon
-                        icon="mdi:open-in-new"
-                        class="project__icons__icon"
-                    />
-                </a>
-            {/if}
-        </div>
-        <div
-            class="project__images"
-            style:--height="{imageSliderHeight}px"
-            bind:this={imageSlider}
+  <div
+    id={project.id}
+    class="project"
+    class:project--active={selected}
+    style:--visible={visible ? "visible" : "hidden"}
+    in:fly={{ x: -200, duration: 800 }}
+    bind:this={element}
+  >
+    <h3 class="project__title">{project.name}</h3>
+    <div class="project__icons">
+      {#if project.urls.yt}
+        <a href={project.urls.yt} target="_blank" rel="noopener">
+          <iconify-icon
+            icon="mdi:youtube"
+            class="project__icons__icon project__icons__icon--yt"
+          />
+        </a>
+      {/if}
+      {#if project.urls.github}
+        <a
+          class="project__icons__icon project__icons__icon--github"
+          href={project.urls.github}
+          target="_blank"
+          rel="noopener"
         >
-            <ImageSlider
-                --object-fit="cover"
-                images={project.images}
-                {duration}
-                delay={imageSlideDelay}
-            />
-        </div>
-        <div class="project__description">
-            {project.description}
-        </div>
-        <div class="project__tech">
-            {#each project.technologies as technology}
-                <img
-                    class="project__tech__img"
-                    alt={technology}
-                    src={technologies[technology]}
-                />
-            {/each}
-        </div>
+          {@html github}
+        </a>
+      {/if}
+      {#if project.urls.site}
+        <a href={project.urls.site} target="_blank" rel="author noopener">
+          <iconify-icon icon="mdi:open-in-new" class="project__icons__icon" />
+        </a>
+      {/if}
     </div>
+    <div
+      class="project__images"
+      style:--height="{imageSliderHeight}px"
+      bind:this={imageSlider}
+    >
+      <ImageSlider
+        --object-fit="cover"
+        images={project.images}
+        {duration}
+        delay={imageSlideDelay}
+      />
+    </div>
+    <div class="project__description">
+      {project.description}
+    </div>
+    <div class="project__tech">
+      {#each project.technologies as technology}
+        <img
+          class="project__tech__img"
+          alt={technology}
+          src={technologies[technology]}
+        />
+      {/each}
+    </div>
+  </div>
 {/key}
 
 <style lang="scss">
-    .project {
-        display: grid;
-        grid-template:
-            "title icons" 3rem
-            "slider slider" 1fr
-            "description description" auto
-            "tech tech" auto / auto 1fr;
-        gap: 10px;
-        padding: 10px 20px 20px;
-        color: white;
-        visibility: var(--visible);
-        background: var(--background);
-        scroll-margin-top: calc(6rem + 0.5rem);
+  .project {
+    display: grid;
+    grid-template:
+      "title icons" 3rem
+      "slider slider" 1fr
+      "description description" auto
+      "tech tech" auto / auto 1fr;
+    gap: 10px;
+    padding: 10px 20px 20px;
+    color: white;
+    visibility: var(--visible);
+    background: var(--background);
+    scroll-margin-top: calc(6rem + 0.5rem);
 
-        &--active {
-            box-shadow: 5px 5px 20px var(--color-theme-3);
-        }
-
-        &__title {
-            align-self: center;
-            margin: 0;
-            font-size: 1.5rem;
-
-            @media (max-width: 850px) {
-                font-size: 1.3rem;
-            }
-        }
-
-        &__icons {
-            display: flex;
-            gap: 10px;
-            align-items: center;
-            justify-self: end;
-
-            &__icon {
-                font-size: 1.8rem;
-                color: white;
-                transition: color 0.5s;
-
-                &:hover {
-                    color: var(--color-theme-1);
-                    transition: color 0.8s;
-                }
-
-                &--github {
-                    width: 2rem;
-                }
-            }
-        }
-
-        &__images {
-            grid-area: slider;
-
-            height: var(--height);
-            transition: height 0.5s;
-        }
-
-        &__description {
-            grid-area: description;
-            margin-top: 10px;
-            font-size: 1.05rem;
-        }
-
-        &__tech {
-            display: flex;
-            flex-wrap: wrap;
-            grid-area: tech;
-            gap: 10px;
-            align-items: end;
-            align-self: end;
-            height: fit-content;
-
-            &__img {
-                height: 30px;
-            }
-        }
+    &--active {
+      box-shadow: 5px 5px 20px var(--color-theme-3);
     }
+
+    &__title {
+      align-self: center;
+      margin: 0;
+      font-size: 1.5rem;
+
+      @media (max-width: 850px) {
+        font-size: 1.3rem;
+      }
+    }
+
+    &__icons {
+      display: flex;
+      gap: 10px;
+      align-items: center;
+      justify-self: end;
+
+      &__icon {
+        font-size: 1.8rem;
+        color: white;
+        transition: color 0.5s;
+
+        &:hover {
+          color: var(--color-theme-1);
+          transition: color 0.8s;
+        }
+
+        &--github {
+          width: 2rem;
+        }
+
+        &--yt {
+          font-size: 2.3rem;
+        }
+      }
+    }
+
+    &__images {
+      grid-area: slider;
+
+      height: var(--height);
+      transition: height 0.5s;
+    }
+
+    &__description {
+      grid-area: description;
+      margin-top: 10px;
+      font-size: 1.05rem;
+    }
+
+    &__tech {
+      display: flex;
+      flex-wrap: wrap;
+      grid-area: tech;
+      gap: 10px;
+      align-items: end;
+      align-self: end;
+      height: fit-content;
+
+      &__img {
+        height: 30px;
+      }
+    }
+  }
 </style>
