4f2e4bb4e0416ece36f60a3fa86bee5ad51c70a0
MarcinSkic <marcinskicoficjal@gmail.com>
2024-10-07 17:11:26 +0200

    feat(blog): add text checks using retext plugins

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,46 @@
 import { mdsvex } from "mdsvex";
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import remarkRetext from "remark-retext";
+import retextEnglish from "retext-english";
+import retextReadability from "retext-readability";
+import retextIndefiniteArticle from "retext-indefinite-article";
+import retextEquality from "retext-equality/en";
+import retextPassive from "retext-passive";
+import retextRepeatedWords from "retext-repeated-words";
+import retextSimplify from "retext-simplify";
+import retextSpell from "retext-spell";
+import { unified } from "unified";
+import reporter from "vfile-reporter";
+import dictionaryEn from "dictionary-en";
 
+/** @type {import("mdsvex").MdsvexOptions} */
 const mdsvexConfig = {
   extensions: [".svx", ".md"],
+  remarkPlugins: [
+    [
+      remarkRetext,
+      unified()
+        .use(retextEnglish)
+        .use(retextReadability, { age: 20 })
+        .use(retextIndefiniteArticle)
+        .use(retextEquality)
+        .use(retextPassive)
+        .use(retextRepeatedWords)
+        .use(retextSimplify)
+        .use(retextSpell, { dictionary: dictionaryEn }),
+    ],
+    reportRetextWarnings,
+  ],
 };
+
+function reportRetextWarnings() {
+  return function (_, file) {
+    if (!file.data.fm.published) {
+      console.warn(reporter(file));
+    }
+  };
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
