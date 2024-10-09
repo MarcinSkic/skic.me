f14c329eea21aff729e92be8173be86bd8f3dc9c
MarcinSkic <marcinskicoficjal@gmail.com>
2024-10-09 12:46:43 +0200

    feat: add posthog analytics

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,1 +1,14 @@
+import posthog from "posthog-js";
+import { browser } from "$app/environment";
+
 export const prerender = true;
+
+export const load = async () => {
+  if (browser) {
+    posthog.init("phc_iyI1iWEuZOLBnyJ26H7GDUGBWS6Fa1nkrArdPAkfQyr", {
+      api_host: "https://eu.i.posthog.com",
+      person_profiles: "never",
+    });
+  }
+  return;
+};
