555bffa558e2cd321c418e7a91f2b066fe6c4169
MarcinSkic <marcinskicoficjal@gmail.com>
2026-06-09 00:54:47 +0200

    feat: add takopi presentation demo

---
+++ src/routes/presentations/takopi/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Takopi's Original Sin - demo prezentacji</title>
+</svelte:head>
+
+<iframe
+  src="/presentations/takopi-slides/index.html"
+  title="Takopi's Original Sin - prezentacja"
+  style="width:100%;height:100vh;border:none;"
+></iframe>
