98ad1b6fecae0ddc079896939eee7e192272c554
MarcinSkic <marcinskicoficjal@gmail.com>
2026-06-19 00:00:49 +0200

    feat: add madoka demo presentation

---
+++ src/routes/presentations/madoka/+page.svelte
@@ -0,0 +1,9 @@
+<svelte:head>
+  <title>Puella Magi Madoka Magica - demo prezentacji</title>
+</svelte:head>
+
+<iframe
+  src="/presentations/madoka-slides/index.html"
+  title="Puella Magi Madoka Magica - prezentacja"
+  style="width:100%;height:100vh;border:none;"
+></iframe>
